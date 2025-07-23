a00e457420918b30b53779fbdf5ef676ef552716
Bellmin <whdals011203@naver.com>
2025-07-23 14:09:04 +0900

    feat: search 기능 추가

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,14 +1,29 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+  }
+
+  const searchParams = useSearchParams();
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
       />
